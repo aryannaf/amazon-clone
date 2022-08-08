7b464fc6fa9f2abf46efcda7655462fbee8d91d4
aryannaf <aryannafarhadi@gmail.com>
2022-08-08 15:11:23 -0400

    header with mobile styling

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Home from './components/Home/Home';
+import Header from './components/Header/Header';
 
 function App() {
   return (
       <BrowserRouter>
         <Switch>
-          <Route path="/" component={Home}/>
+          {/* <Route path="/" component={Home}/> */}
+          <Route path="/" component={Header}/>
         </Switch>
       </BrowserRouter>
   );
